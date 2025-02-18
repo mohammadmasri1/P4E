a42d475fe48a2a36851ea24285701b646dafa620
Mohammad E.R. Masri <mm031292@gmail.com>
2025-02-18 14:00:34 +0300

    Changes to be committed: 	modified:   Attendance.py 	new file:   app,.py 	new file:   templates/index.html

---
+++ app,.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, abort, render_template
+
+app = Flask(__name__)
+
+# List of allowed IP addresses (replace with your lab's IP range)
+allowed_ips = ['192.168.1.0', '192.168.1.255']
+
+@app.before_request
+def limit_remote_addr():
+    if request.remote_addr not in allowed_ips:
+        abort(403)  # Forbidden
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
